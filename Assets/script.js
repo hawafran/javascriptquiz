98d7472cfff9460f6dc5059fbfc95a678534756a
Hawa <hawa.francis@gmail.com>
2021-04-19 12:14:07 -0400

    html, css, js pseudo code

---
+++ Assets/script.js
@@ -0,0 +1,15 @@
+// initialize quiz
+
+
+// timer begins 
+// user presented with questions
+// user clicks from a choice of answers
+// computer alerts user whether correct or incorrect
+    // if answer is incorrect, time is subtracted 
+// answer is stored
+// computer moves to the next question after answer is solected 
+// quiz is over when timer hits 0 or all questions are asnwered 
+// user presented with score 
+// user saves score with initials
+    // computer stores score
+// computer shows users score alone with other saved scores 
