dba8b3beef16e3771378aec280740918c9383ae7
Hawa <hawa.francis@gmail.com>
2021-04-19 20:06:30 -0400

    completed HTML, first function

--- Assets/script.js
+++ Assets/script.js
@@ -2,8 +2,80 @@
 
 
 // timer begins 
-// user presented with questions
-// user clicks from a choice of answers
+
+var correct=0;
+var choiceA, choiceB, choiceC, choiceD;
+var choices;
+var userChoice;
+
+let questions = [
+    {
+        question: "question 1",
+        choiceA: "A",
+        choiceB: "B",
+        choiceC: "C",
+        choiceD: "D",
+        correctAnswer: "C"
+      },
+    {
+        question: "question 2",
+        choiceA: "A",
+        choiceB: "B",
+        choiceC: "C",
+        choiceD: "D",
+        correctAnswer: "B"
+      },
+    {
+        question: "question 3",
+        choiceA: "A",
+        choiceB: "B",
+        choiceC: "C",
+        choiceD: "D",
+        correctAnswer: "A"
+      },
+    {
+        question: "question 4",
+        a: "A",
+        b: "B",
+        c: "C",
+        d: "D",
+        correctAnswer: "A"
+      },
+      {
+        question: "question 5",
+      a: "A",
+      b: "B",
+      c: "C",
+      d: "D",
+      correctAnswer: "C"
+    },
+
+    {
+        question: "question 6",
+      a: "A",
+      b: "B",
+      c: "C",
+      d: "D",
+      correctAnswer: "D"
+    }
+  ];
+
+
+// user is presented with test questions
+let lastQuestionIndex=questions.length-1;
+let runningQuestionIndex=0;
+
+function renderQuestion(){
+  let x = questions[runningQuestionIndex];
+question.innerHTML="<p>"+ x.question +"</p>";
+choiceA.innerHTML=x.choiceA;
+choiceB.innerHTML=x.choiceB;
+choiceC.innerHTML=x.choiceC;
+choiceD.innerHTML=x.choiceD;
+}
+
+
+
 // computer alerts user whether correct or incorrect
     // if answer is incorrect, time is subtracted 
 // answer is stored
@@ -13,3 +85,22 @@
 // user saves score with initials
     // computer stores score
 // computer shows users score alone with other saved scores 
+
+
+// function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
+
+// 	function showQuestions(questions, quizContainer){
+// 		// code will go here
+// 	}
+
+// 	function showResults(questions, quizContainer, resultsContainer){
+// 		// code will go here
+// 	}
+
+// 	// show the questions
+// 	showQuestions(questions, quizContainer);
+
+// 	// when user clicks submit, show results
+// 	submitButton.onclick = function(){
+// 		showResults(questions, quizContainer, resultsContainer);
+// 	}
